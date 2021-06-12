41219cd7fddbd29bee9e86de5f545bbd68f14c5b
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2021-06-12 17:17:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,22 +8,19 @@ cd /tmp/recovery
 
 sudo apt install git -y
 
-#repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault
 
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
+# repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
 repo sync -j$(nproc --all)
 
-
-# git clone https://github.com/Dazzler555/surtwr.git -b vas-jok device/xiaomi/surya
-# git clone https://github.com/Brock5555/shrp_device_xiaomi_surya.git -b twrp device/xiaomi/surya
-git clone https://github.com/Jjtitdutitdtti7s5s/twsurbrg.git -b twrp device/xiaomi/surya
+git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git device/xiaomi/violet
 rm -rf out
 
-. build/envsetup.sh && lunch omni_surya-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
+. build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 
-cd out/target/product/surya
+cd out/target/product/violet
 
 curl -sL https://git.io/file-transfer | sh 
 
